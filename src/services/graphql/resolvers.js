01708af0c60d2746e8466f506e38f6ce86261d93
David Herrmann <davherrmann@googlemail.com>
2017-05-04 23:22:55 +0100

    lazy-load services in resolvers

--- src/services/graphql/resolvers.js
+++ src/services/graphql/resolvers.js
@@ -3,10 +3,10 @@ import request from 'request-promise'
 export default function Resolvers () {
   let app = this
 
-  let Posts = app.service('posts')
-  let Users = app.service('users')
-  let Comments = app.service('comments')
-  let Viewer = app.service('viewer')
+  let Posts = () => app.service('post')
+  let Users = () => app.service('user')
+  let Comments = () => app.service('comment')
+  let Viewer = () => app.service('viewer')
 
   const localRequest = request.defaults({
     baseUrl: `http://${app.get('host')}:${app.get('port')}`,
@@ -16,7 +16,7 @@ export default function Resolvers () {
   return {
     User: {
       posts (user, args, context) {
-        return Posts.find({
+        return Posts().find({
           query: {
             authorId: user._id
           }
@@ -25,19 +25,19 @@ export default function Resolvers () {
     },
     Post: {
       comments (post, { limit }, context) {
-        return Comments.find({
+        return Comments().find({
           query: {
             postId: post._id
           }
         })
       },
       author (post, args, context) {
-        return Users.get(post.authorId)
+        return Users().get(post.authorId)
       }
     },
     Comment: {
       author (comment, args, context) {
-        return Users.get(comment.authorId)
+        return Users().get(comment.authorId)
       }
     },
     AuthPayload: {
@@ -47,33 +47,33 @@ export default function Resolvers () {
     },
     RootQuery: {
       viewer (root, args, context) {
-        return Viewer.find(context)
+        return Viewer().find(context)
       },
       author (root, { username }, context) {
-        return Users.find({
+        return Users().find({
           query: {
             username
           }
         }).then((users) => users[0])
       },
       authors (root, args, context) {
-        return Users.find({})
+        return Users().find({})
       },
       posts (root, { category }, context) {
-        return Posts.find({
+        return Posts().find({
           query: {
             category
           }
         })
       },
       post (root, { _id }, context) {
-        return Posts.get(_id)
+        return Posts().get(_id)
       }
     },
 
     RootMutation: {
       signUp (root, args, context) {
-        return Users.create(args)
+        return Users().create(args)
       },
       logIn (root, {username, password}, context) {
         return localRequest({
@@ -83,16 +83,16 @@ export default function Resolvers () {
         })
       },
       createPost (root, {post}, context) {
-        return Posts.create(post, context)
+        return Posts().create(post, context)
       },
       createComment (root, args, context) {
-        return Comments.create(args, context)
+        return Comments().create(args, context)
       },
       removePost (root, { _id }, context) {
-        return Posts.remove(_id, context)
+        return Posts().remove(_id, context)
       },
       removeComment (root, { _id }, context) {
-        return Comments.remove(_id, context)
+        return Comments().remove(_id, context)
       }
     }
 
